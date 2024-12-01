fb0edfaa9763ab94edc2a93890e9fd560ec724f8
Kaio Rocha <kaiodtr@gmail.com>
2024-12-01 19:59:14 -0300

    refactor: enhance `wait-for-postgres.js` script with timeout and waiting spinner

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,20 +1,60 @@
 const { exec } = require("node:child_process");
 
+const startTime = Date.now();
+const spinnerIntervalMs = 200;
+const timeoutMs = 300000; // 5 min
+
 function checkPostgres() {
-  exec("docker exec postgres-dev pg_isready -h localhost", handleReturn);
+  showWaitingMessage();
 
-  function handleReturn(error, stdout) {
-    if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
-      checkPostgres();
-      return;
-    }
+  exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    (error, stdout) => {
+      if (Date.now() - startTime > timeoutMs) {
+        process.stderr.write(
+          "\n🔴 Timeout: PostgreSQL is not accepting connections.\n",
+        );
+        process.exit(1);
+      }
 
-    process.stdout.write(
-      "\n🟢 PostgreSQL is ready and accepting connections!\n\n",
-    );
-  }
+      if (stdout.search("accepting connections") === -1) {
+        process.stdout.write(".");
+        checkPostgres();
+        return;
+      }
+
+      process.stdout.write(
+        "\n🟢 PostgreSQL is ready and accepting connections!\n\n",
+      );
+    },
+  );
 }
 
-process.stdout.write("\n🟡 Waiting for PostgreSQL to accept connections");
+function showWaitingMessage() {
+  const spinner = [
+    "🕐",
+    "🕑",
+    "🕒",
+    "🕓",
+    "🕔",
+    "🕕",
+    "🕖",
+    "🕗",
+    "🕘",
+    "🕙",
+    "🕚",
+    "🕛",
+  ];
+
+  const spinnerIndex =
+    Math.floor(Date.now() / spinnerIntervalMs) % spinner.length;
+
+  const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+
+  process.stdout.write(
+    `\r${spinner[spinnerIndex]} ` +
+      `Waiting for PostgreSQL to accept connections...\t${elapsedTime}s`,
+  );
+}
+
 checkPostgres();
